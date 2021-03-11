3a927e68dcfe2ff40e9c01c90f3d1e2931ad88d7
JoeBellWork <JoeBellWork@users.noreply.github.com>
2021-03-11 19:32:27 +0000

    Added Parallax scrolling background
    To finish, button events

---
+++ ReadySteadyKnight/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector2(transform.position.x + .1f, transform.position.y);
+    }
+}
---
+++ ReadySteadyKnight/Assets/Scripts/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public GameObject camObj;
+    public float parallaxEffect, speed;
+
+    private float mapLength, startPos, distance, temp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        mapLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        temp = camObj.transform.position.x * (1 - parallaxEffect);
+        distance = camObj.transform.position.x * parallaxEffect;
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(temp > startPos + mapLength)
+        {
+            startPos += mapLength;
+        }
+        else if(temp < startPos - mapLength)
+        {
+            startPos -= mapLength;
+        }
+    }
+}
