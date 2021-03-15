4cb8b137956e8996dfee5482729e8057443fdad6
JoeBellWork <JoeBellWork@users.noreply.github.com>
2021-03-15 13:05:34 +0000

    Reset Layer order for background images

---
+++ ReadySteadyKnight/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    
+
+    public void PlayButton1()
+    {
+
+    }
+
+    public void PlayButton2()
+    {
+
+    }
+
+    public void QuitGameButton()
+    {
+
+    }
+}
